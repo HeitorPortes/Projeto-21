0f2452df65aa31a77df6dfc4b302b5359a6f3091
HeitorPortes <132620493+HeitorPortes@users.noreply.github.com>
2023-08-03 22:25:38 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,33 +2,37 @@ var PLAY = 1;
 var END = 0;
 var GameState = PLAY;
 
-var tucano, tucano_voando, tucano_space, tucano_collided;
+var tucano, tucano_voando, tucano_collided;
 var ground, groundGif;
 
 var nuvemGroup, nuvemGif;
 var obstaculosGroup, carcara, coruja, bemTV;
 
-var invisibleBlock;
+var invisibleBlock, invisibleBlock2;
 
 var score;
 
-var restartGif, gameOverGif;
+var jumpSound, dieSound;
+
+var restartGif, gameOverPng, gameOver, restart;
 
 function preload(){
-    tucano_voando = loadImage("tucano1.gif");
-    tucano_space = loadAnimation("tucano2.gif");
-    tucano_collided = loadImage("tucano3.gif");
+    tucano_voando = loadImage("Tucano.gif");
+    tucano_collided = loadImage("tucano2.png");
 
     groundGif = loadImage("Ground.gif");
 
-    nuvemGif = loadImage("nuvem.gif");
+    nuvemGif = loadImage("nuvem.png");
 
-    carcara = loadImage("carcara.gif");
-    coruja = loadImage("coruja.gif");
-    bemTV = loadImage("bemtv.gif");
+    carcara = loadImage("carcara1.png");
+    coruja = loadImage("Coruja.png");
+    bemTV = loadImage("BemTv.png");
 
-    restartGif = loadImage("restart.gif");
-    gameOverGif = loadImage("gameOver.gif");
+    restartGif = loadImage("restart.png");
+    gameOverPng = loadImage("gameOver.png");
+
+    jumpSound = loadSound("jump.mp3");
+    dieSound = loadSound("tucanoDead.mp3");
 
 }
 
@@ -40,31 +44,34 @@ function setup() {
   ground.x = ground.width /2;
   ground.scale=1000000000000;
     
-    tucano = createSprite(50,100,20,20);
-    tucano.addImage("voando",tucano_voando);
+    tucano = createSprite(35,100,20,20);
+    tucano.addImage(tucano_voando);
     
-    tucano.scale = 4;
+    tucano.scale = 2;
 
-    invisibleBlock=createSprite(300,200,1000,10);
+    invisibleBlock=createSprite(300,220,1000,10);
     invisibleBlock.visible = false;
 
-   
+   invisibleBlock2=createSprite(300,0.0001,1000,1);
+   invisibleBlock2.visible = false;
 
-    gameOver = createSprite(300,100);
-  gameOver.addImage(gameOverGif);
+  gameOver = createSprite(300,100,20,20);
+  gameOver.addImage(gameOverPng);
+  gameOver.scale = 100;
 
-  restart = createSprite(300,140);
+  restart = createSprite(300,140,20,20);
   restart.addImage(restartGif);
+  restart.scale = 3;
 
   gameOver.scale = 0.5;
   restart.scale = 0.5;
 
-  obstaclesGroup = createGroup();
-  cloudsGroup = createGroup();
-  
-  console.log("Hello" + 5);
+  obstaculosGroup = createGroup();
+  nuvemGroup = createGroup();
 
   score = 0;
+
+  tucano.setCollider("circle",0,0,10);
 
 }
 
@@ -73,9 +80,7 @@ function draw() {
   
   text("Score: "+ score, 500,50);
   
-  console.log(GameState);
-
-
+  
 
   if(GameState === PLAY){
     gameOver.visible = false
@@ -89,14 +94,10 @@ function draw() {
     if (ground.x < 0){
       ground.x = ground.width/2;
     }
-    
-    if(keyWentDown("space")&& tucano.y >= 100) {
-        tucano.velocityY = -12;
-        tucano.addAnimation("space",tucano_space);
-    }
 
-    if(keyWentUp("space")){
-      tucano.addImage("voando",tucano_voando);
+    if(keyDown("SPACE")){
+      tucano.velocityY = -10;
+      jumpSound.play();
     }
     
     tucano.velocityY = tucano.velocityY + 0.8
@@ -105,30 +106,37 @@ function draw() {
   
     spawnObstacles();
     
-    if(obstaclesGroup.isTouching(tucano)){
-        gameState = END;
+    if(obstaculosGroup.isTouching(tucano)){
+        GameState = END;
+        dieSound.play();
     }
 
-    if(invisibleBlock.isTouching(tucano)){
-      gameState = END;
+    if(invisibleBlock.isTouching(tucano) || invisibleBlock2.isTouching(tucano)){
+      GameState = END;
+      dieSound.play();
     }
   
   }
-   else if (gameState === END) {
-     console.log("hey")
+   else if (GameState === END) {
+     
       gameOver.visible = true;
       restart.visible = true;
+
+      ground.velocityX = 0
      
-      ground.velocityX = 0;
       tucano.velocityY = 0
      
-      tucano.changeAnimation("collided", tucano_collided);
+      tucano.addImage(tucano_collided);
      
-    obstaclesGroup.setLifetimeEach(-1);
+    obstaculosGroup.setLifetimeEach(-1);
     nuvemGroup.setLifetimeEach(-1);
      
-     obstaclesGroup.setVelocityXEach(0);
+     obstaculosGroup.setVelocityXEach(0);
      nuvemGroup.setVelocityXEach(0);
+   }
+
+   if(mousePressedOver(restart)){
+    reset();
    }
 
     drawSprites();
@@ -140,19 +148,20 @@ function reset(){
     GameState = PLAY;
     gameOver.visible = false;
     restart.visible = false;
-
-    tucano.addImage("collided",tucano_collided);
   
-    obstaclesGroup.destroyEach();
-    cloudsGroup.destroyEach();
+    tucano.addImage(tucano_voando);
+    tucano.y = 100;
+
+    obstaculosGroup.destroyEach();
+    nuvemGroup.destroyEach();
     score = 0;
   
-    trex.changeAnimation("running", trex_running);
+    tucano.changeImage(tucano_voando);
   }
 
   function spawnObstacles(){
     if (frameCount % 60 === 0){
-      var obstacle = createSprite(400,165,10,40);
+      var obstacle = createSprite(600,110,10,40);
       obstacle.velocityX = -(6+score/1000);
       
        var rand = Math.round(random(1,3));
@@ -166,26 +175,23 @@ function reset(){
          default: break;
        }
 
-       obstacle.scale = 1.5;
+       obstacle.scale = 2;
        obstacle.lifetime = 300;
       
-       obstaclesGroup.add(obstacle);
+       obstaculosGroup.add(obstacle);
     }
    }
 
    function spawnClouds() {
     if (frameCount % 60 === 0) {
-       nuvem = createSprite(600,100,40,10);
-      nuvem.y = Math.round(random(10,60));
+       var nuvem = createSprite(600,110,10,40);
+      nuvem.y = Math.round(random(1));
       nuvem.addImage(nuvemGif);
-      nuvem.scale = 0.5;
+      nuvem.scale = 3;
       nuvem.velocityX = -3;
       
-      nuvem.lifetime = 134;
+      nuvem.lifetime = 300;
       
-      nuvem.depth = tucano.depth;
-      tucano.depth = tucano.depth + 1;
-      
-     cloudsGroup.add(nuvem);
+     nuvemGroup.add(nuvem);
       }
   }
